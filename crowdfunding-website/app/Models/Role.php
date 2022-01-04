c7e62ea27e42e3b04f6a52ed6b6382b7ceb43ec7
Vincent Nathaniel M <vincentmoeljopranoto@gmail.com>
2022-01-04 23:22:57 +0700

    UUID

---
+++ crowdfunding-website/app/Models/Role.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Role extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function($model){
+            if(empty($model->{$model->getKeyName()}))
+            {
+                $model->{$model->getKeyName()} = Str::uuid();
+            }
+        });
+    }
+}
---
+++ crowdfunding-website/database/migrations/2022_01_04_160824_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('username')->unique();
+            $table->string('email')->unique();
+            $table->string('name');
+            $table->uuid('role_id');
+            $table->foreign('role_id')->references('id')->on('roles');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
